eda7a46807724f900b03af325ca7b44af441be1d
Shubh Pratap Singh <149113566+ShubhPS@users.noreply.github.com>
2025-05-04 16:00:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/courses', courseRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('public'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+        res.sendFile(path.join(__dirname, 'views', 'index', 'index.html'));
     });
 }
 
